0e24bedf218aaa8075c595d48bd7b9e1ada6f6f5
alexjons987 <alexander.jonsson987@outlook.com>
2025-11-03 14:43:43 +0100

    Add task 1

---
+++ springbasics/src/main/java/com/example/springbasics/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springbasics;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    HelloService helloService;
+
+    public HelloController() {
+        helloService = new HelloService();
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name) {
+        return helloService.greet(name);
+    }
+}
---
+++ springbasics/src/main/java/com/example/springbasics/HelloService.java
@@ -0,0 +1,7 @@
+package com.example.springbasics;
+
+public class HelloService {
+    public String greet(String name) {
+        return String.format("Hello %s!", name);
+    }
+}
