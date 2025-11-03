7ec4f6666eb938ce9534015182f2f03088416139
alexjons987 <alexander.jonsson987@outlook.com>
2025-11-03 15:55:36 +0100

    Add completed task 4

--- springbasics/src/main/java/com/example/springbasics/controller/PersonController.java
+++ springbasics/src/main/java/com/example/springbasics/controller/PersonController.java
@@ -1,14 +1,35 @@
 package com.example.springbasics.controller;
 
 import com.example.springbasics.model.Person;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class PersonController {
-    @PostMapping("/person")
+    private List<Person> personList = new ArrayList<>();
+
+    @PostMapping("/printperson")
     public String createPerson(@RequestBody Person person) {
         return person.toString();
+    }
+
+    @PostMapping("/addperson")
+    public String addPerson(@RequestBody Person person) {
+        try {
+            personList.add(person);
+        } catch (Exception ignored) {
+            return "Failed to add person!";
+        }
+        return "Added person to list!";
+    }
+
+    @GetMapping("/people")
+    public List<Person> getAllPersons() {
+        return personList;
     }
 }
